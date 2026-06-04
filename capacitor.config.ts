b8bab423d22a9fb12629dd3cbe96b941ea460c8a
pau.sabe <pau.sabe@icloud.com>
2026-06-04 15:04:51 +0200

    change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.biblegames.eden',
-  appName: 'Eden Choice Chronicles',
+  appName: ' Bible Story Game: Sacred Quest',
   webDir: 'dist',
   ios: {
     contentInset: 'automatic'
