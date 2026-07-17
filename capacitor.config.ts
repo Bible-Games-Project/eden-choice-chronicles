8f957598c9b80ddc33f196a8397ca68d9cad8fcb
biblegamesproject <121623683+biblegamesproject@users.noreply.github.com>
2026-07-17 09:22:07 +0200

    chore: update app name to "Bible Unlocked" via bgp-admin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.biblegames.eden',
-  appName: 'Sacred Quest',
+  appName: 'Bible Unlocked',
   webDir: 'dist',
   ios: {
     contentInset: 'automatic',
